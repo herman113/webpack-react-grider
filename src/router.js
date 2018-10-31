1061706215f26594d788f2ea1381a96d3800fcf3
Herman Soto <hermansoto@me.com>
2018-10-31 14:12:16 -0400

    Convert routes toa javascript object named componentRoutes

--- src/router.js
+++ src/router.js
@@ -7,6 +7,35 @@ import ArtistDetail from './components/artists/ArtistDetail';
 import ArtistCreate from './components/artists/ArtistCreate';
 import ArtistEdit from './components/artists/ArtistEdit';
 
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes: [
+    {
+      path: 'artists/new',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistCreate')
+          .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistDetail')
+          .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistEdit')
+          .then(module => cb(null, module.default));
+      }
+    },
+  ]
+};
+
 const Routes = () => {
   return (
     <Router history={hashHistory}>
